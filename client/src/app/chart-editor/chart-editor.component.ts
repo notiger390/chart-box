e1856cc7f628a2715fe06796888dd759ac39bd79
notiger390 <notiger390@gmail.com>
2025-10-26 10:00:33 +0900

    ページ構成を修正

---
+++ client/src/app/chart-editor/chart-editor.component.ts
@@ -0,0 +1,10 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-editor',
+  imports: [],
+  templateUrl: './chart-editor.component.html',
+  styleUrls: ['./chart-editor.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ChartEditorComponent {}
--- client/src/app/sample-list/sample-list.component.ts
+++ client/src/app/sample-list/sample-list.component.ts
@@ -2,10 +2,10 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-sample-list',
   imports: [RouterLink],
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  templateUrl: './sample-list.component.html',
+  styleUrls: ['./sample-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeComponent {}
+export class SampleListComponent {}
